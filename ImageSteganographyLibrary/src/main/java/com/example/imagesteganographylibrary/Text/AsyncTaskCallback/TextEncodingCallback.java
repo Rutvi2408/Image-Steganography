0a3d4a5f0cdf44e55e4d1bc649ecf4f55f2d241b
Rutvi Pan <panrutvi97@gmail.com>
2021-04-22 10:00:20 +0530

    Image-Steganography

---
+++ ImageSteganographyLibrary/src/main/java/com/example/imagesteganographylibrary/Text/AsyncTaskCallback/TextEncodingCallback.java
@@ -0,0 +1,15 @@
+package com.example.imagesteganographylibrary.Text.AsyncTaskCallback;
+
+import com.example.imagesteganographylibrary.Text.ImageSteganography;
+
+/**
+ * This the callback interface for TextEncoding AsyncTask.
+ */
+
+public interface TextEncodingCallback {
+
+    void onStartTextEncoding();
+
+    void onCompleteTextEncoding(ImageSteganography result);
+
+}
